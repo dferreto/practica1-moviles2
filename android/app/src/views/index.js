1279dec170e7d55647af19269e28a39a89ccf67b
dferreto <diferretomo@est.utn.ac.cr>
2024-02-02 17:42:41 -0600

    Modificación final del index.js

--- android/app/src/views/index.js
+++ android/app/src/views/index.js
@@ -4,55 +4,89 @@ import React from 'react';
 
 		const data = [
 			{
+				logo: require('../imgs/signos/Aries.png'),
 				nombre : 'Aries',
 				dia  : '21 de marzo al 19 de abril',
-				creditos : 6},
+				elementos : 'Fuego',
+				astro : 'Marte',
+				piedra : 'Jaspe rojo y rubí'},
 			{
+				logo: require('../imgs/signos/Tauro.png'),
 				nombre : 'Tauro',
 				dia  : '20 de abril al 20 de mayo',
-				creditos : 2},
+				elementos : 'Tauro',
+				astro : 'Venus',
+				piedra : 'Amatista y cuarzo rosa'},
 			{
+				logo: require('../imgs/signos/Geminis.png'),
 				nombre : 'Géminis',
 				dia  : '21 de mayo al 20 de junio',
-				creditos : 3},
+				elementos : 'Aire',
+				astro : 'Mercurio',
+				piedra : 'Berilo y ámbar'},
 			{
+				logo: require('../imgs/signos/Cancer.png'),
 				nombre : 'Cáncer',
 				dia  : '21 de junio al 22 de julio',
-				creditos : 6},
+				elementos : 'Agua',
+				astro : 'Luna',
+				piedra : 'Cristal de roca, perla y plata'},
 			{
+				logo: require('../imgs/signos/Leo.png'),
 				nombre : 'Leo',
 				dia  : '23 de julio al 22 de agosto',
-				creditos : 0},
+				elementos : 'Fuego',
+				astro : 'Sol',
+				piedra : 'Topacio, crisoberilo, ámbar amarillo y ojo de tigre'},
 			{
+				logo: require('../imgs/signos/Virgo.png'),
 				nombre : 'Virgo',
-				dia  : 2,
-				creditos : 6},
+				dia  : ' 23 de agosto al 22 de septiembre',
+				elementos : 'Tierra',
+				astro : 'Mercurio',
+				piedra : 'Aventurina y topacio'},
 			{
+				logo: require('../imgs/signos/Libra.png'),
 				nombre : 'Libra',
-				dia  : 2,
-				creditos : 3},
+				dia  : '23 de septiembre al 22 de octubre',
+				elementos : 'Aire',
+				astro : 'Venus',
+				piedra : 'Ágata y jade'},
 			{
+				logo: require('../imgs/signos/Scorpio.png'),
 				nombre : 'Escorpio',
-				dia  : 2,
-				creditos : 3},
+				dia  : '23 de octubre al 21 de noviembre',
+				elementos : 'Agua',
+				astro : 'Plutón',
+				piedra : 'Coral y jaspe sardo'},
 			{
+				logo: require('../imgs/signos/Sagitario.png'),
 				nombre : 'Sagitario',
-				dia  : 2,
-				creditos : 3},
+				dia  : '22 de noviembre al 21 de diciembre',
+				elementos : 'Fuego',
+				astro : 'Júpiter',
+				piedra : 'Calcedonia, esmeralda y turquesa'},
 			{
+				logo: require('../imgs/signos/Capricornio.png'),
 				nombre : 'Capricornio',
-				dia  : 2,
-				creditos : 0},
+				dia  : '22 de diciembre al 19 de enero',
+				elementos : 'Tierra',
+				astro : 'Saturno',
+				piedra : 'Diamante y obsidiana'},
             {
+				logo: require('../imgs/signos/Acuario.png'),
                 nombre : 'Acuario',
-                dia  : 2,
-                creditos : 3},
+                dia  : '20 de enero al 18 de febrero',
+                elementos : 'Aire',
+				astro : 'Urano',
+				piedra : 'Zafiro'},
             {
+				logo: require('../imgs/signos/Piscis.png'),
                 nombre : 'Piscis',
-                dia  : 2,
-                creditos : 3}
-
-				
+                dia  : '19 de febrero al 20 de marzo',
+                elementos : 'Agua',
+				astro : 'Neptuno',
+				piedra : 'Aguamarina y amatista'}
 		];
 
 		const Index = () => {
@@ -61,17 +95,31 @@ import React from 'react';
 					<View style={style_01.divHeader}>
 						<Image source={require('../imgs/logos/logo_universidad.png')} />
 					</View>
-
+					
+					
 					<View style={style_01.divMain}>
 						<Text style={style_01.h1}>Signos Zodiacales</Text>
+						<View>
+					</View>
+						<ScrollView style={{ marginTop: 8}}>
+    {data.map(item => (<View style={style_01.tarjeta} key={item.nombre}>
+            <View style={{ flexDirection: 'row' }}>
+                <Image
+                    source={item.logo}
+                    style={{ width: 70, height: 70, marginRight: 10 }}
+                /> 
+                <View>
+                    <Text>Nombre: {item.nombre}</Text>
+                    <Text>Días: {item.dia}</Text>
+                    <Text>Elemento: {item.elementos}</Text>
+                    <Text>Astro celeste: {item.astro}</Text>
+                    <Text>Piedra preciosa: {item.piedra}</Text>
+                </View>
+            </View>
+        </View>
+    ))}
+</ScrollView>
 
-						<ScrollView style={{marginTop:8}}>
-							{data.map(item => <View style={style_01.tarjeta} key={item.nombre} >
-								<Text>Nombre: {item.nombre}</Text>
-								<Text>Días: {item.dia}</Text>
-								<Text>Créditos: {item.creditos}</Text>
-							</View>)}
-						</ScrollView>
 					</View>
 
 					<View style={style_01.divFooter}>
